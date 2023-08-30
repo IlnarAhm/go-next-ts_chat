d4c74bf08174e20d98b6c0435bf49256b1886e76
IlnarAhm <ilnar.ahma@yandex.ru>
2023-08-30 17:50:09 +0300

    ws routes and methods

--- server/router/router.go
+++ server/router/router.go
@@ -2,18 +2,24 @@ package router
 
 import (
 	"server/internal/user"
+	"server/internal/ws"
 
 	"github.com/gin-gonic/gin"
 )
 
 var r *gin.Engine
 
-func InitRouter(userHanlder *user.Handler) {
+func InitRouter(userHanlder *user.Handler, wsHandler *ws.Handler) {
 	r = gin.Default()
 
 	r.POST("/signup", userHanlder.CreateUser)
 	r.POST("/login", userHanlder.Login)
 	r.GET("/logout", userHanlder.Logout)
+
+	r.POST("/ws/createRoom", wsHandler.CreateRoom)
+	r.GET("/ws/joinRoom/:roomId", wsHandler.JoinRoom)
+	r.GET("/ws/getRooms", wsHandler.GetRooms)
+	r.GET("/ws/getClients/:roomId", wsHandler.GetClients)
 }
 
 func Start(addr string) error {
